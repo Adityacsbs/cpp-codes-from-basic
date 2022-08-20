46f74529ce8353cbd3c130620e34ca3664dbeaac
AADITYARAJ SINGH CHOUHAN <94135988+Adityacsbs@users.noreply.github.com>
2022-08-21 00:30:05 +0530

    Add files via upload

---
+++ 12accessstring.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   string ok="hello my name is adityaraj singh chouhan";
+
+   cout<<ok[4]<<endl;
+   cout<<ok[3]<<endl;
+
+    return 0;
+}
