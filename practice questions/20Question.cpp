7c047ac45d5ae32132e1c0351fc3072e569169c3
AADITYARAJ SINGH CHOUHAN <94135988+Adityacsbs@users.noreply.github.com>
2022-09-06 22:40:16 +0530

    Update 20Question.cpp

--- practice questions/20Question.cpp
+++ practice questions/20Question.cpp
@@ -1,10 +1,23 @@
-//30. Write a program in C++ to compute the total and average of four numbers. 
-#include<iostream>
+ #include<iostream>
 
 using namespace std;
 int main (){
+    int a,b;
+    //1. Write a C++ program to compute the sum of the two given integer values. 
+    //If the two values are the same, then return triple their sum. 
+      cout<<"enter the two numbers :- ";
+      cin>>a>>b;
 
-
+    if(a==b){
+        int c;
+        c=(a+b)*3;
+        cout<<c;
+    }
+    else{
+        int d;
+        d=a+b;
+        cout<<d;
+    }
 
 
     return 0;
